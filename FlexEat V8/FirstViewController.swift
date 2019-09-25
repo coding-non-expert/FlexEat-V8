4042e585620f8b117b9e924ed2ce6b14a5180d4f
celeste <celiresmart@gmail.com>
2019-09-25 20:21:16 +0800

    Initial Commit

---
+++ FlexEat V8/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FlexEat V8
+//
+//  Created by Celeste Tan on 25/9/19.
+//  Copyright © 2019 cuzwhynot. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
