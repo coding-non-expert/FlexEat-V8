f128d924e3a5eea15131f8f034e8e84222ce9848
celeste <celiresmart@gmail.com>
2019-11-13 12:38:27 +0800

    - set up basics of goal page and segues - removed constraints on profile page for easy moving of parts later

---
+++ FlexEat V8/GoalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GoalViewController.swift
+//  FlexEat V8
+//
+//  Created by Celeste Tan on 13/11/19.
+//  Copyright © 2019 cuzwhynot. All rights reserved.
+//
+
+import UIKit
+
+class GoalViewController: UIViewController {
+
+    @IBOutlet weak var backToHomePageButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
