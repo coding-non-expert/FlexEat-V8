f0b94394927bbbb046f9b5544a95ec42a24e1439
celeste <celiresmart@gmail.com>
2019-11-13 11:16:03 +0800

    - created profile page - linked profile page using segues from the profile button - activated camera and choose from library function - back button segued to home page - constraints not yet complete - username is under dummy data

---
+++ FlexEat V8/ProfileViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ProfileViewController.swift
+//  FlexEat V8
+//
+//  Created by Celeste Tan on 13/11/19.
+//  Copyright © 2019 cuzwhynot. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var usernameDisplayLabel: UILabel!
+    @IBOutlet weak var backToHomePageButton: UIButton!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var changeProfilePhotoButton: UIButton!
+    
+    var username = "Henrietta"
+    var profileImage: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        usernameDisplayLabel.text = username
+        profileImageView.image = UIImage(named: profileImage)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let defaults = UserDefaults.standard
+        profileImage = defaults.string(forKey: "profileImage") ?? ""
+        profileImageView.image = UIImage(named: profileImage)
+    }
+    
+    @IBAction func changeProfilePhotoButtonTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true,
+                             completion: nil)
+            }
+            alertController.addAction(cameraAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { action in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.originalImage] as? UIImage else { return }
+        
+        let generatedName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(generatedName)
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        profileImage = imagePath.path
+        let defaults = UserDefaults.standard
+        defaults.set(profileImage, forKey: "profileImage")
+        profileImageView.image = UIImage(named: profileImage)
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
